2d5a2777ef4a2a4f53d61526d1fae3dea9fb9ce0
skthegawd <skrybe@gmail.com>
2025-02-10 18:01:24 -0500

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,1 +1,13 @@
+export default function Sidebar() {
+    return (
+        <aside className="sidebar">
+            <h2>Vader AI</h2>
+            <ul>
+                <li>Dashboard</li>
+                <li>AI Chat</li>
+                <li>Settings</li>
+            </ul>
+        </aside>
+    );
+}
 
