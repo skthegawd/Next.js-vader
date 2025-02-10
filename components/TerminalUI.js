2bef3005973b15fa70b6d58a908eb2472ec8db46
skthegawd <skrybe@gmail.com>
2025-02-10 18:01:10 -0500

    Create TerminalUI.js

---
+++ components/TerminalUI.js
@@ -0,0 +1,7 @@
+export default function TerminalUI() {
+    return (
+        <div className="terminal">
+            <p>Lord Vader, your AI assistant is at your command.</p>
+        </div>
+    );
+}
