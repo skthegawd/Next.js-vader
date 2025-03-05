260dc5d6a6eedb16d84b374f5033d21ab3fd08f2
skthegawd <skrybe@gmail.com>
2025-03-05 17:59:58 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -30,6 +30,15 @@ module.exports = {
                     },
                 ],
             },
+            {
+                source: "/_next/static/:path*",
+                headers: [
+                    {
+                        key: "Cache-Control",
+                        value: "public, max-age=31536000, immutable",
+                    },
+                ],
+            },
         ];
     },
 
