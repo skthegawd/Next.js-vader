68c1a4b0ddbec714d7ac1c75fe9ed0acd33eee30
skthegawd <skrybe@gmail.com>
2025-04-30 20:58:34 -0400

    cache

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
         return [
             {
                 source: '/api/:path*',
-                destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/:path*`,
+                destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/:path*`,
             },
         ];
     },
